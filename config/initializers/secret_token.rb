67a3d6aec8e20cb5b6f5d3a0e2ee53d06b1adfa4
David Berko <davidberko19@gmail.com>
2012-08-16 09:27:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainingVideoDummy::Application.config.secret_token = '28f80caa514dce33576c96113b0227cdbe42993ea4158a981e13cf996d7da3273469cf990b3a4a8fef9aff08609143fa2d2ce7e9ec85f195602b638f4d655238'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TrainingVideoDummy::Application.config.session_store :cookie_store, key: '_TrainingVideoDummy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TrainingVideoDummy::Application.config.session_store :active_record_store
